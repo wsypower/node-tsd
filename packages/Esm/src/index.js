7e44c91d92aa945b3147e0bc5cae35f8e1ae6cf4
weiyafei <2553241022@qq.com>
2023-02-06 01:13:04 +0800

    feat: assert json

--- packages/Esm/src/index.js
+++ packages/Esm/src/index.js
@@ -1,3 +1,11 @@
 import pck from '../package.json' assert { type: 'json' }
+import c from './c.cjs'
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+
+const testPck = require('../package.json');
+const testCjs = require('./c.cjs');
+console.log(testCjs)
+console.log(c)
 
 console.log(pck)
